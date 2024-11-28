cbb410dd74f251117128c1fe56d88ff59657186a
Geert Vanderkelen <geert.vanderkelen@kelvin.green>
2024-11-28 18:01:40 +0100

    feat: upgrade parser and ProvidedFieldsForCurrentMutation
    We upgrade the gqlparser to v2.
    
    `ProvidedFieldsForCurrentMutation` returns exactly what
     `GQLGenInputObjectFields` returned.
    
    Tests were added using the parser and actual GraphQL queries.
    
    The GQLGenInputObjectFields is marked as deprecated.

--- input_object_test.go
+++ input_object_test.go
@@ -4,192 +4,151 @@ package gogql_test
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"testing"
 
 	"github.com/99designs/gqlgen/graphql"
-	"github.com/golistic/xt"
+	"github.com/golistic/xgo/xt"
+	"github.com/vektah/gqlparser/v2"
 	"github.com/vektah/gqlparser/v2/ast"
-	"github.com/vektah/gqlparser/v2/parser"
 
 	"github.com/golistic/gogql"
 )
 
-//type TestUserModel struct {
+// type TestUserModel struct {
 //	ID    int    `json:"id"`
 //	Name  string `json:"name"`
 //	Email string `json:"email"`
-//}
+// }
 //
-//type UpdateTestUserModelInput struct {
+// type UpdateTestUserModelInput struct {
 //	ID    int    `json:"id"`
 //	Name  string `json:"name"`
 //	Email string `json:"email"`
-//}
+// }
 
 var schema = `
 schema {
+    query: Query
     mutation: Mutation
 }
 
+type Query {
+	user(id: ID!): User
+}
+
 type Mutation {
-	updateUser(input: UpdateUserInput!): User
+	updateUser(input: UpdateUserInput!, settings: SettingsInput): User
 }
 
 type User {
-  id: ID!
-  name: String!
-  email: String!
+	id: ID!
+	name: String!
+	email: String!
 }
 
 input UpdateUserInput {
-  id: ID!
-  name: String
-  givenName: String
-  email: String
+	id: ID!
+	name: String
+	givenName: String
+	email: String
+}
+
+input SettingsInput {
+	notify: Boolean
+	newsletter: Boolean
 }
 `
-
-func testContext(t *testing.T, query string, vars gogql.Variables, args []string, ctx context.Context) context.Context {
-	srcMutation := &ast.Source{
-		Name:    "test",
-		Input:   query,
-		BuiltIn: false,
-	}
-
-	doc, err := parser.ParseQuery(srcMutation)
-	xt.OK(t, err)
-	_ = doc
-
-	rqCtx := &graphql.OperationContext{
-		RawQuery:             query,
-		Variables:            vars,
-		Doc:                  doc,
-		OperationName:        doc.Operations[0].Name,
-		DisableIntrospection: true,
-		ResolverMiddleware: func(ctx context.Context, next graphql.Resolver) (res interface{}, err error) {
-			return nil, nil
-		},
-		RootResolverMiddleware: func(ctx context.Context, next graphql.RootResolver) graphql.Marshaler {
-			return nil
-		},
-		Operation: doc.Operations[0],
-	}
-
-	fldCtx := &graphql.FieldContext{
-		Parent: nil,
-		Object: "",
-		Args:   nil,
-		Field: graphql.CollectedField{
-			Field: &ast.Field{
-				SelectionSet: doc.Operations[0].SelectionSet[0].(*ast.Field).SelectionSet,
-			},
-		},
-		Index:      nil,
-		Result:     nil,
-		IsMethod:   false,
-		IsResolver: false,
-		Child:      nil,
-	}
-
-	xt.OK(t, rqCtx.Validate(ctx))
-
-	ctx = graphql.WithOperationContext(ctx, rqCtx)
-	ctx = graphql.WithFieldContext(ctx, fldCtx)
-
-	return ctx
-}
 
 func TestGQLGenInputObjectFields(t *testing.T) {
 	srcSchema := &ast.Source{
 		Name:    "TestSchema",
 		Input:   schema,
-		BuiltIn: false,
+		BuiltIn: true,
 	}
-	schemaDoc, err := parser.ParseSchema(srcSchema)
+
+	schemaAst, err := gqlparser.LoadSchema(srcSchema)
 	xt.OK(t, err)
-	_ = schemaDoc
 
 	var cases = map[string]struct {
 		query     string
-		exp       map[string][]string
-		expErr    error
+		want      map[string][]string
+		wantErr   error
 		args      []string
-		variables gogql.Variables
+		variables map[string]any
 	}{
 		"with operation name": {
 			query: `mutation UpdateUser { updateUser(input: {id: 123, name: "Marta"}) { id } }`,
-			exp:   map[string][]string{"input": {"id", "name"}},
+			want:  map[string][]string{"input": {"id", "name"}},
+			args:  []string{"input"},
 		},
 		"multiple arguments": {
-			query: `mutation { updateUser(input: {id: 123, name: "Marta"}, input2: {foo: "bar"}) { id } }`,
-			exp:   map[string][]string{"input": {"id", "name"}, "input2": {"foo"}},
-			args:  []string{"input", "input2"},
+			query: `mutation { updateUser(input: {id: 123, name: "Marta"}, settings: {notify: true}) { id } }`,
+			want:  map[string][]string{"input": {"id", "name"}, "settings": {"notify"}},
+			args:  []string{"input", "settings"},
+		},
+		"multiple arguments (get all)": {
+			query: `mutation { updateUser(input: {id: 123, name: "Marta"}, settings: {notify: true}) { id } }`,
+			want:  map[string][]string{"input": {"id", "name"}, "settings": {"notify"}},
 		},
 		"argument missing from field arguments": {
 			query: `mutation { updateUser(input: {id: 123, name: "Marta"}) { id } }`,
-			exp:   map[string][]string{"input": {"id", "name"}},
-			args:  []string{"input", "input2"},
+			want:  map[string][]string{"input": {"id", "name"}},
+			args:  []string{"input", "settings"},
 		},
-		"using variables": {
-			query: `mutation ($input2: UpdateUserInput!) { updateUser(input: {id: 123, name: "Marta"}, input2: $input2) { id } }`,
-			exp:   map[string][]string{"input": {"id", "name"}, "input2": {"foo"}},
-			args:  []string{"input", "input2"},
-			variables: gogql.Variables{
-				"input2": gogql.Variables{"foo": "bar"},
+		"using variables for one argument": {
+			query: `mutation ($settings: SettingsInput!) { updateUser(input: {id: 123, name: "Marta"}, settings: $settings) { id } }`,
+			want:  map[string][]string{"input": {"id", "name"}, "settings": {"newsletter"}},
+			args:  []string{"input", "settings"},
+			variables: map[string]any{
+				"settings": map[string]any{"newsletter": false},
 			},
 		},
 		"input argument is object": {
-			query: `mutation { updateUser(input: {id: 123, name: "Marta"}, foo: 1234) { id } }`,
-			exp:   map[string][]string{"input": {"id", "name"}},
-			args:  []string{"input", "foo"},
-		},
-		"no input arguments": {
-			query: `mutation { updateUser(foo: 1234) { id } }`,
-			exp:   nil,
+			query: `mutation { updateUser(input: {id: 123, name: "Marta"}) { id } }`,
+			want:  map[string][]string{"input": {"id", "name"}},
 			args:  []string{"input", "foo"},
 		},
 		"not a mutation": {
-			query:  `query { user(id: 123) { id } }`,
-			exp:    nil,
-			expErr: fmt.Errorf("field context missing"),
-		},
-		"multiple mutations": {
-			query: `
-mutation ($inputA: UpdateUserInput!, $inputB: UpdateUserInput!) {
-	updateUser(input: $inputA) { id }
-	updateUser(input: $inputB) { id }
-}
-`,
-			exp: map[string][]string{"inputA": {"id", "name"}, "inputB": {"givenName", "id"}},
-			variables: gogql.Variables{
-				"inputA": gogql.Variables{"id": "123", "name": "Marta"},
-				"inputB": gogql.Variables{"id": "987", "givenName": "Tomas"},
-			},
-		},
-		"multiple mutations but no variables": {
-			query: `
-mutation {
-	updateUser(input: {id: "123", name: "Marta"}) { id }
-	updateUser(input: {id: "987", givenName: "Tomas"}) { id }
-}
-`,
-			exp: map[string][]string{"inputA": {"id", "name"}, "inputB": {"givenName", "id"}},
+			query:   `query { user(id: 123) { id } }`,
+			want:    nil,
+			wantErr: fmt.Errorf("not a mutation"),
 		},
 	}
 	for name, c := range cases {
 		t.Run(name, func(t *testing.T) {
-			ctx := context.Background()
-			ctx = testContext(t, c.query, c.variables, c.args, ctx)
-			fmt.Println("### args", c.args)
-			have, err := gogql.GQLGenInputObjectFields(ctx, c.args...)
+			doc, gqlErrors := gqlparser.LoadQuery(schemaAst, c.query)
+			if gqlErrors != nil {
+				t.Fatal("cannot load schema", gqlErrors.Error())
+			}
 
-			if c.expErr != nil {
-				xt.Eq(t, c.expErr, errors.Unwrap(err))
+			// recreate the context like used by gqlgen
+			operation := doc.Operations[0]
+			field := operation.SelectionSet[0].(*ast.Field)
+
+			opCtx := &graphql.OperationContext{
+				Variables: c.variables,
+				Operation: operation,
+			}
+
+			fieldCtx := &graphql.FieldContext{
+				Field: graphql.CollectedField{
+					Field: field,
+				},
+			}
+
+			ctx := context.Background()
+			ctx = graphql.WithOperationContext(ctx, opCtx)
+			ctx = graphql.WithFieldContext(ctx, fieldCtx)
+
+			// test using arguments
+			have, err := gogql.ProvidedFieldsForCurrentMutation(ctx, c.args...)
+
+			if c.wantErr != nil {
+				xt.ErrorIs(t, c.wantErr, err)
 			} else {
 				xt.OK(t, err)
-				xt.Eq(t, c.exp, have)
+				xt.Eq(t, c.want, have)
 			}
 		})
 	}
